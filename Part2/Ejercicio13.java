d8bcc1ba60293783274c58ecb31ab160bfb44593
Sergio Castell <sergiocastell@users.noreply.github.com>
2016-12-05 17:13:55 +0100

    Update Ejercicio13.java
    Con la resolución de clase incluida (parte original comentada)

--- Part2/Ejercicio13.java
+++ Part2/Ejercicio13.java
@@ -10,39 +10,83 @@ import utilidades.Teclado;
 
 public class Ejercicio13 {
     public static void main(String[] args) {
-        System.out.println("Introduzca el día en el que nació (2 cifras)");
-        int dia = Teclado.readInt();
-        if (dia >= 1 && dia <= 31) {
-            System.out.println("Introduzca el mes en el que nació (2 cifras)");
-            int mes = Teclado.readInt();
-            if (mes >= 1 && mes <= 12) {
-                System.out.println("Introduzca el año en el que nació (4 cifras)");
-                int antildeo = Teclado.readInt();
-                // probablemente la peor (pesadilla) optimización de variables de la historia. don't kill me pls 
-                int dia_2 = dia%10;
-                dia = dia/10;
-                int dia_1 = dia%10;
-                int mes_2 = mes%10;
-                mes = mes/10;
-                int mes_1 = mes%10;
-                int antildeo_4 = antildeo%10;
-                antildeo = antildeo/10;
-                int antildeo_3 = antildeo%10;
-                antildeo = antildeo/10;
-                int antildeo_2 = antildeo%10;
-                antildeo = antildeo/10;
-                int antildeo_1 = antildeo%10;
-                int suerte_temp = antildeo_1 + antildeo_2 + antildeo_3 + antildeo_4 + mes_1 + mes_2 + dia_1 + dia_2;
-                int suerte_2 = suerte_temp%10;
-                suerte_temp = suerte_temp/10;
-                int suerte_1 = suerte_temp%10;
-                int suerte = suerte_2+suerte_1;
-                System.out.println("Su número de la suerte es el " + suerte);
-            } else {
-                System.out.println("Pruebe de nuevo");
+//        System.out.println("Introduzca el día en el que nació (2 cifras)");
+//        int dia = Teclado.readInt();
+//        if (dia >= 1 && dia <= 31) {
+//            System.out.println("Introduzca el mes en el que nació (2 cifras)");
+//            int mes = Teclado.readInt();
+//            if (mes >= 1 && mes <= 12) {
+//                System.out.println("Introduzca el año en el que nació (4 cifras)");
+//                int antildeo = Teclado.readInt();
+//                // probablemente la peor (pesadilla) optimización de variables de la historia. don't kill me pls 
+//                int dia_2 = dia%10;
+//                dia = dia/10;
+//                int dia_1 = dia%10;
+//                int mes_2 = mes%10;
+//                mes = mes/10;
+//                int mes_1 = mes%10;
+//                int antildeo_4 = antildeo%10;
+//                antildeo = antildeo/10;
+//                int antildeo_3 = antildeo%10;
+//                antildeo = antildeo/10;
+//                int antildeo_2 = antildeo%10;
+//                antildeo = antildeo/10;
+//                int antildeo_1 = antildeo%10;
+//                int suerte_temp = antildeo_1 + antildeo_2 + antildeo_3 + antildeo_4 + mes_1 + mes_2 + dia_1 + dia_2;
+//                int suerte_2 = suerte_temp%10;
+//                suerte_temp = suerte_temp/10;
+//                int suerte_1 = suerte_temp%10;
+//                int suerte = suerte_2+suerte_1;
+//                System.out.println("Su número de la suerte es el " + suerte);
+//            } else {
+//                System.out.println("Pruebe de nuevo");
+//            }
+//        } else {
+//            System.out.println("Pruebe de nuevo");
+//        }
+        String fecha;
+        String[] aux;
+        System.out.println("Introduzca su fecha de nacimiento");
+        fecha = Teclado.readString();
+        aux = fecha.split("/");
+        String cifras = aux[0]+aux[1]+aux[2];
+        //System.out.println(aux[0]+aux[1]+aux[2]);
+        // probablemente la peor (pesadilla) optimización de variables de la historia. don't kill me pls 
+//      int dia = Integer.parseInt(aux[0]);
+//      int mes = Integer.parseInt(aux[1]);
+//      int anyo = Integer.parseInt(aux[2]);
+        int sucifras;
+        do {
+            sucifras=0;
+            for (int i=0; i<cifras.length();i++){
+                 char c = cifras.charAt(i);
+                 int valor = Character.getNumericValue(c);
+                 //System.out.println(valor);
+                 sucifras += valor;
             }
-        } else {
-            System.out.println("Pruebe de nuevo");
-        }
+            cifras = Integer.toString(sucifras);
+            
+        } while (sucifras>=10);
+        
+//                int dia_2 = dia%10;
+//                dia = dia/10;
+//                int dia_1 = dia%10;
+//                int mes_2 = mes%10;
+//                mes = mes/10;
+//                int mes_1 = mes%10;
+//                int antildeo_4 = antildeo%10;
+//                antildeo = antildeo/10;
+//                int antildeo_3 = antildeo%10;
+//                antildeo = antildeo/10;
+//                int antildeo_2 = antildeo%10;
+//                antildeo = antildeo/10;
+//                int antildeo_1 = antildeo%10;
+//                int suerte_temp = antildeo_1 + antildeo_2 + antildeo_3 + antildeo_4 + mes_1 + mes_2 + dia_1 + dia_2;
+//                int suerte_2 = suerte_temp%10;
+//                suerte_temp = suerte_temp/10;
+//                int suerte_1 = suerte_temp%10;
+//                int suerte = suerte_2+suerte_1;
+                
+                System.out.println("Su número de la suerte es el " + sucifras);
     }
 }
